24e449caa6461e7f3016c1c5474fca2bcbedcef9
Art <ya.Artriches@ya.ru>
2021-01-02 13:58:08 +0300

    add: base theme material-ui

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,4 +1,14 @@
 import App from "next/app";
-import React, { Component } from "react";
+import React from "react";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { ThemeProvider } from "@material-ui/core/styles";
+import { themeDark, themeLight } from "lib/theme";
 
-export default ({ Component, pageProps }) => <Component {...pageProps} />;
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <ThemeProvider theme={false ? themeDark : themeLight}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
+}
